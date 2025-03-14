db19680d2aa2ede2609a3fa751c64e1117f78bf2
TropheusJ <jverrellijr@icloud.com>
2025-03-14 03:25:28 -0400

    allow placement on interfaces

--- src/main/java/io/github/cichlidmc/distmarker/Distribution.java
+++ src/main/java/io/github/cichlidmc/distmarker/Distribution.java
@@ -7,12 +7,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Indicates that a class or member is exclusive to a specific distribution.
+ * Indicates that the annotated element is exclusive to a specific distribution.
  * This comes with no behavior, it is purely documentation.
  */
 @Documented
 @Retention(RetentionPolicy.SOURCE)
-@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.ANNOTATION_TYPE})
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.ANNOTATION_TYPE, ElementType.TYPE_USE})
 public @interface Distribution {
 	Dist value();
 }
